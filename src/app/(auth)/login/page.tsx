cf1b391ef588ee835bfb7c8925c17259f093f966
peemknn <kunanontpeem333@gmail.com>
2023-11-15 21:59:10 +0700

    add form path and component

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,6 @@
+import LoginForm from "@/components/Auth/LoginForm";
+import Link from "next/link";
+
+export default function LoginPage() {
+  return <LoginForm />;
+}
---
+++ src/components/Auth/LoginForm.tsx
@@ -0,0 +1,46 @@
+"use client";
+import { Input } from "@/components/ui/input";
+import { Label } from "@radix-ui/react-label";
+import LargeLogo from "@/../public/assets/logo/logo.svg";
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "../ui/button";
+export default function LoginForm() {
+  return (
+    <div className="flex flex-col justify-center items-center min-h-screen w-screen">
+      <div className="flex flex-col gap-20 pb-8 justify-center items-center">
+        <Link href="/">
+          <Image src={LargeLogo} alt="logo-img"></Image>
+        </Link>
+        <h1 className="text-md font-bold">Log in</h1>
+      </div>
+      <div className="pb-4 flex w-full flex-col items-center ">
+        <div className="form-control w-full max-w-xs">
+          <Label className="text-md" htmlFor="email">
+            Email
+          </Label>
+          <Input
+            className="w-full rounded-md max-w-xs text-sm"
+            type="email"
+            placeholder="ex. john.smith@email.com"
+          />
+        </div>
+      </div>
+      <div className="pb-4 flex w-full flex-col items-center ">
+        <div className="form-control w-full max-w-xs">
+          <Label className="text-md" htmlFor="password">
+            Password
+          </Label>
+          <Input
+            className="w-full rounded-md  text-sm"
+            type="password"
+            placeholder="Enter your password"
+          />
+        </div>
+      </div>
+      <Button variant="default" size="sm">
+        Login
+      </Button>
+    </div>
+  );
+}

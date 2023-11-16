21d36da0476b247bf5f2cb4c30b4083eafc8d5e6
peemknn <kunanontpeem333@gmail.com>
2023-11-16 19:57:14 +0700

    add /hotels style

--- src/app/(sidetab)/hotel/page.tsx
+++ src/app/(sidetab)/hotel/page.tsx
@@ -4,6 +4,60 @@ import { Input } from "@/components/ui/input";
 import Image from "next/image";
 
 export default function Hotel() {
+  const mockHotelData = [
+    {
+      name: "S31 Sukhumvit Bangkok",
+      price: 1500,
+      location: "Bangkok, Wattana",
+    },
+    {
+      name: "Grand Hotel",
+      price: 1200,
+      location: "City Center, Downtown",
+    },
+    {
+      name: "Ocean View Resort",
+      price: 1800,
+      location: "Beachside, Paradise Island",
+    },
+    {
+      name: "Mountain Retreat Lodge",
+      price: 1000,
+      location: "Serenity Valley",
+    },
+    {
+      name: "Riverside Inn",
+      price: 1300,
+      location: "Scenic Riverfront",
+    },
+    {
+      name: "Luxury Oasis Spa & Resort",
+      price: 2500,
+      location: "Desert Oasis",
+    },
+    {
+      name: "Alpine Chalet",
+      price: 1600,
+      location: "Snowy Mountains",
+    },
+    {
+      name: "Historic Manor House",
+      price: 1900,
+      location: "Countryside Estates",
+    },
+    {
+      name: "Tropical Paradise Hotel",
+      price: 2000,
+      location: "Island Haven",
+    },
+    {
+      name: "Urban Boutique Suites",
+      price: 1400,
+      location: "Metropolitan District",
+    },
+    // Add more hotel objects as needed
+  ];
+
   return (
     <div className="font-inter">
       <form>
@@ -21,9 +75,13 @@ export default function Hotel() {
       </form>
       <div>
         <h1 className="font-bold mb-3">All Hotels</h1>
-        <div className="w-max-xs">
+        <div className="w-max-xs gap-3 flex flex-wrap">
           {" "}
-          <HotelCard />
+          {mockHotelData.map((hotel, index) => (
+            <div key={index} className="md:w-[300px]  w-full">
+              <HotelCard {...hotel} />
+            </div>
+          ))}
         </div>
       </div>
     </div>
--- src/components/HotelCard/HotelCard.tsx
+++ src/components/HotelCard/HotelCard.tsx
@@ -1,27 +1,35 @@
 import Image from "next/image";
 
-const HotelCard = () => {
+const HotelCard = ({
+  name,
+  price,
+  location,
+}: {
+  name: string;
+  price: number;
+  location: string;
+}) => {
   return (
-    <div className="bg-white shadow-lg flex flex-col gap-2 p-3 rounded-xl w-fit font-inder">
+    <div className="bg-white shadow-lg flex flex-col gap-2 p-3 rounded-xl w-full  font-inder overflow-hidden">
       <Image
         src={"/images/hotels/bangkok_s31_sukhumvit.jpeg"}
         alt="hotel"
         width={0}
         height={0}
         sizes="100vw"
-        className="w-[290px] h-[180px] rounded-lg"
+        className="w-auto md:h-[180px] h-[220px] rounded-lg"
       />
       <div className="flex flex-row justify-between items-center py-1">
         <div>
-          <h1 className="text-lg"> S31 Sukhumvit Bangkok </h1>
+          <h1 className="text-lg"> {name} </h1>
           <h2> ⭐️⭐️⭐️⭐️⭐️ </h2>
         </div>
         <div className="flex flex-col items-end bg-slate-100 p-1 rounded-xl">
-          <p className="text-lg font-bold font-inter"> 1,500 ฿ </p>
+          <p className="text-lg font-bold font-inter"> {price} ฿ </p>
           <p className="text-xs"> per night </p>
         </div>
       </div>
-      <p className="text-xs py-2"> 📍 Bangkok, Watthana </p>
+      <p className="text-xs py-2"> 📍 {location} </p>
     </div>
   );
 };

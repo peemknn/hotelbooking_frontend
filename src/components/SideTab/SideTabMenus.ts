0d00d69af673da760c2e1f77a119ac46fa99c227
peemknn <kunanontpeem333@gmail.com>
2023-11-21 00:03:49 +0700

    change sidetabmenu path

--- src/components/SideTab/SideTabMenus.ts
+++ src/components/SideTab/SideTabMenus.ts
@@ -1,5 +1,5 @@
 export const SideTabMenus: { label: string; href: string }[] = [
-  { label: "Hotel", href: "/places?search=01" },
-  { label: "Booking", href: "/external-links" },
-  { label: "About Us", href: "/whyus" },
+  { label: "Hotel", href: "/hotels" },
+  { label: "Booking", href: "/bookings" },
+  { label: "About Us", href: "/aboutus" },
 ];

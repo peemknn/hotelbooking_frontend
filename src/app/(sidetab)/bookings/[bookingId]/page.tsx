627062fd2234a98268fe99fb038f3e9be51c7d5a
peemknn <kunanontpeem333@gmail.com>
2023-11-21 12:28:09 +0700

    add booking by id

---
+++ src/app/(sidetab)/bookings/[bookingId]/page.tsx
@@ -0,0 +1,32 @@
+"use client";
+import getBooking from "@/lib/applibs/bookings/getBooking";
+import { useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
+import { convertToFulldate } from "@/utils/date";
+
+export default function BookingPageById({
+  params,
+}: {
+  params: { bookingId: string };
+}) {
+  const { data: session } = useSession();
+  const [bookingDesc, setBookingDesc] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const bookingDesc = await getBooking(
+        params.bookingId,
+        session?.user.token
+      );
+      setBookingDesc(bookingDesc);
+    };
+    fetchData();
+  }, []);
+
+  if (!bookingDesc) return <h1>Loading...</h1>;
+  return (
+    <div className="w-full flex flex-col">
+      <div>Checkin: {convertToFulldate(bookingDesc.data.bookingDate)}</div>
+    </div>
+  );
+}

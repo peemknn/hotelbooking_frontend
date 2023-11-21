1766113aca99dd386c462e41930f15d16d11a39d
peemknn <kunanontpeem333@gmail.com>
2023-11-21 15:23:33 +0700

    added create hotels func

---
+++ src/lib/applibs/hotels/addHotel.tsx
@@ -0,0 +1,14 @@
+export default async function addHotel(data: any, token: any) {
+  const res = await fetch(process.env.NEXT_PUBLIC_API_URL + "/hotels", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      authorization: `Bearer ${token}`,
+    },
+    body: data,
+  });
+  if (!res.ok) {
+    throw new Error("Failed to add hotel");
+  }
+  return res.json();
+}

8a2c37bd75d73ddcbd02615467f0a94e8222e88b
peemknn <kunanontpeem333@gmail.com>
2023-11-22 16:51:31 +0700

    add /aboutus page

---
+++ src/app/(sidetab)/aboutus/page.tsx
@@ -0,0 +1,14 @@
+export default function AboutUsPage() {
+  return (
+    <div className="flex flex-col items-center w-full font-mono">
+      <div className="m-10 text-center space-y-2">
+        <h1 className="text-xl font-bold">SW DEV PRACII</h1>
+        <h3 className="text-md font-bold text-md">Hotel Booking</h3>
+      </div>
+      <div className="space-y-4 text-md">
+        <p>6231306621 Kunanont Kaewkhampa</p>
+        <p>6230362521 Patcharapong Khempan</p>
+      </div>
+    </div>
+  );
+}
